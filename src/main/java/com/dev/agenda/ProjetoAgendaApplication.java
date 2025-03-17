846ca83062b47fc771f8d76d7e25a8e898635760
pablolleite88 <pablol.leite88@gmail.com>
2025-03-17 09:47:23 -0300

    projeto-agenda

---
+++ src/main/java/com/dev/agenda/ProjetoAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/agenda/ProjetoAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
